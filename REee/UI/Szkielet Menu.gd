b88062d54d99c8cbe201858cdb5d51ee49e7c116
Reniferion <JakubRychlikofficial@gmail.com>
2024-01-16 05:23:01 +0100

    Szkielety do Menusów
    tylko klikanie
    Play przenosi do "LeveL"
    Escape zamyka
    reszta przenosi do menusów na razie bez funkcjonalnosci

---
+++ REee/UI/Szkielet Menu.gd
@@ -0,0 +1,20 @@
+extends Control
+
+func _on_play_pressed(): 
+ get_tree().change_scene_to_file("res//level.tscn")
+
+
+func _on_options_pressed():	
+
+ get_tree().change_scene_to_file("res://UI/szkielet_menu_opcji.tscn")
+
+
+
+
+func _on_credits_pressed():
+   get_tree().change_scene_to_file("res://UI/Credits.tscn")
+
+
+
+func _on_exit_pressed(): 
+   get_tree().quit()
